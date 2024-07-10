7e514dbb1d5ac2215e1a6b68a6c91997be016cc7
FilledOfCode <103237408+FilledOfCode@users.noreply.github.com>
2024-07-10 10:33:00 +0800

    add documentations

--- phow_caltech101.py
+++ phow_caltech101.py
@@ -10,3 +10,15 @@ from glob import glob
 from random import sample, seed
 from scipy import ones, mod, arange, array, where, ndarray, hstack, linspace, histogram, vstack, amax, amin
 from scipy.misc import imread, imresize
+from scipy.cluster.vq import vq
+import numpy
+from vl_phow import vl_phow
+from vlfeat import vl_ikmeans
+from scipy.io import loadmat, savemat
+from sklearn import svm
+from sklearn.metrics import confusion_matrix, accuracy_score
+import pylab as pl
+from datetime import datetime
+from sklearn.kernel_approximation import AdditiveChi2Sampler
+from cPickle import dump, load
+
